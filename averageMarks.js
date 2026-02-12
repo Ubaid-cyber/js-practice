8ec54fca711c6fba1bc993dc27c060a2c684d6a0
Ubaid Ahmad <bhatubaid341@gmail.com>
2026-02-12 13:14:56 +0530

    AverageMarks

---
+++ averageMarks.js
@@ -0,0 +1,12 @@
+let averageMarks = [85,97,44,37,76,60];
+
+let sum = 0;
+
+
+for (let i=0; i<averageMarks.length;i++){
+
+    sum = sum+averageMarks[i];
+
+}
+
+console.log(sum);
---
+++ loops.js
@@ -0,0 +1,16 @@
+let heros = ["uabid", "ahmad","bhat","kullar"];
+
+for(let i=0; i<heros.length;i++){
+
+    console.log(heros[i]);
+}
+
+// for-of loop 
+
+
+let el = ["uabid","dehradhun","srinagar"];
+
+for (let ell of el ){
+
+    console.log(ell.toUpperCase());
+}
